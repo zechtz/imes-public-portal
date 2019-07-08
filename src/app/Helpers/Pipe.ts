f3cb0eec62a0f5d6ce0c924831bb6893cc7dad5e
Zachariah Ngonyani <zachariahn@gmail.com>
2019-07-08 06:48:38 +0300

    Create a pipe helper

---
+++ src/app/Helpers/Pipe.ts
@@ -0,0 +1,8 @@
+import { PipeTransform, Pipe } from '@angular/core';
+
+@Pipe({ name: 'pairs' })
+export class PairsPipe implements PipeTransform {
+  transform(value:any) {
+    return value.filter((v,i) => i%2==0).map((v,i) => [value[i*2], value[i*2+1]])
+  }
+}
