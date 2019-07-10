ce40301c50e1c1a201ebd5ee8d844a3aca3fdd6d
Zachariah Ngonyani <zachariahn@gmail.com>
2019-07-10 10:55:58 +0300

    format helpers

--- src/app/Helpers/util.ts
+++ src/app/Helpers/util.ts
@@ -2,56 +2,56 @@ import isObject from 'lodash/isObject';
 
 // validation
 export function orNull(param) {
-    return param === undefined ? null : param;
+  return param === undefined ? null : param;
 }
 
 export function orArray(param) {
-    return Array.isArray(param) ? param : [];
+  return Array.isArray(param) ? param : [];
 }
 
 export function orObject(param) {
-    return isObject(param) ? param : {};
+  return isObject(param) ? param : {};
 }
 
 // array
 export function arrayGetById(array, id) {
-    return array.find(item => item.id === id);
+  return array.find(item => item.id === id);
 }
 
 // object
 export function arrayToIdMap(array) {
-    return array.reduce((obj, item) => {
-        obj[item.id] = item;
-        return obj;
-    }, {});
+  return array.reduce((obj, item) => {
+    obj[item.id] = item;
+    return obj;
+  }, {});
 }
 
 // date
 export function getDate() {
-    const y = Math.floor(Math.random() * 3) + 2015;
-    const M = Math.floor(Math.random() * 12) + 1;
-    const d = Math.floor(Math.random() * 28) + 1;
-    // const h = Math.floor(Math.random() * 23) + 1;
-    // const m = Math.floor(Math.random() * 59) + 1;
-    // const s = Math.floor(Math.random() * 59) + 1;
+  const y = Math.floor(Math.random() * 3) + 2015;
+  const M = Math.floor(Math.random() * 12) + 1;
+  const d = Math.floor(Math.random() * 28) + 1;
+  // const h = Math.floor(Math.random() * 23) + 1;
+  // const m = Math.floor(Math.random() * 59) + 1;
+  // const s = Math.floor(Math.random() * 59) + 1;
 
-    return new Date(`${y}-${M}-${d}`)
-        .toJSON()
-        .replace('T', ' ')
-        .substr(0, 10);
+  return new Date(`${y}-${M}-${d}`)
+    .toJSON()
+    .replace('T', ' ')
+    .substr(0, 10);
 }
 
 export const formatDate = (value, uiLocale) => {
-    if (typeof global.Intl !== 'undefined' && Intl.DateTimeFormat) {
-        const locale = uiLocale || 'en';
-        return new Intl.DateTimeFormat(locale, {
-            year: 'numeric',
-            month: 'short',
-            day: 'numeric',
-        }).format(new Date(value));
-    }
+  if (typeof global.Intl !== 'undefined' && Intl.DateTimeFormat) {
+    const locale = uiLocale || 'en';
+    return new Intl.DateTimeFormat(locale, {
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+    }).format(new Date(value));
+  }
 
-    return value.substr(0, 19).replace('T', ' ');
+  return value.substr(0, 19).replace('T', ' ');
 };
 
 /**
@@ -64,17 +64,17 @@ export const formatDate = (value, uiLocale) => {
 
 // reducer validator
 export const validateReducer = (value, defaultValue) =>
-    value === undefined || value === null ? defaultValue : value;
+  value === undefined || value === null ? defaultValue : value;
 
 export const getBaseUrl = d2 => {
-    const api = d2.Api.getApi();
-    const idx = api.baseUrl.indexOf('/api');
-    return idx > -1 ? api.baseUrl.slice(0, idx) : api.baseUrl;
+  const api = d2.Api.getApi();
+  const idx = api.baseUrl.indexOf('/api');
+  return idx > -1 ? api.baseUrl.slice(0, idx) : api.baseUrl;
 };
 
 export const getWithoutId = object => ({
-    ...object,
-    id: undefined,
+  ...object,
+  id: undefined,
 });
 
 export const asyncForEach = async(array, callback) => {
