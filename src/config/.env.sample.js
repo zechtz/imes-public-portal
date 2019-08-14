259837d1bc719e1fef55f11848cc0d468e979966
Zachariah Ngonyani <zachariahn@gmail.com>
2019-08-14 13:53:02 +0300

    Create .env.sample file

---
+++ src/config/.env.sample.js
@@ -0,0 +1,7 @@
+export const env = {
+  USERNAME   : 'username',
+  PASSWORD   : 'password',
+  BASE_URL   : 'http://imes.tamisemi.go.tz',
+  PUBLIC_URL : 'http://imes.tamisemi.go.tz',
+  VERSION    : '31'
+}
